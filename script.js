782edc83aba9a9746a7856a83845d36f8ed713a4
Lautaro <lu4ult@gmail.com>
2022-10-22 19:51:42 -0300

    script se ejecuta con botón

--- script.js
+++ script.js
@@ -19,40 +19,42 @@ function obtenerElMayor(primero,segundo,tercero) {
 }
 
 
-for(let i=1;i<=cantidadDeProductos;i++) {
-    let entradaUsuario;
-    do {                                                                    //Validamos que no ingrese 0 como precio.
-        entradaUsuario = parseInt(prompt("Ingrese el precio del producto "+i+" de "+cantidadDeProductos));
-        if(entradaUsuario <= 0)
-            alert("😕😕😕😕\nNo puedo aceptar productos sin precio!\nIntentemos otra vez...");
-    } while(entradaUsuario <= 0);
+const iniciar = () => {
+    for(let i=1;i<=cantidadDeProductos;i++) {
+        let entradaUsuario;
+        do {                                                                    //Validamos que no ingrese 0 como precio.
+            entradaUsuario = parseInt(prompt("Ingrese el precio del producto "+i+" de "+cantidadDeProductos));
+            if(entradaUsuario <= 0)
+                alert("😕😕😕😕\nNo puedo aceptar productos sin precio!\nIntentemos otra vez...");
+        } while(entradaUsuario <= 0);
 
-   switch(i) {
-        case 1: {
-            precio1 = entradaUsuario;
-            // mayorPrecio = entradaUsuario;                                   //Tenemos que asignarles valores la primera vez que se ingresa un dato sino quedan como "undefined"
+       switch(i) {
+            case 1: {
+                precio1 = entradaUsuario;
+                // mayorPrecio = entradaUsuario;                                   //Tenemos que asignarles valores la primera vez que se ingresa un dato sino quedan como "undefined"
+                menorPrecio = entradaUsuario;
+                break;
+            }
+            case 2: {
+                precio2 = entradaUsuario;
+                break;
+            }
+            case 3: {
+                precio3 = entradaUsuario;
+                break;
+            }
+            default:
+                alert("Ups! Algo salió mal...")
+        }
+
+        if(entradaUsuario < menorPrecio)
             menorPrecio = entradaUsuario;
-            break;
+
+        // if(entradaUsuario > mayorPrecio)
+        //     mayorPrecio = entradaUsuario;
         }
-        case 2: {
-            precio2 = entradaUsuario;
-            break;
-        }
-        case 3: {
-            precio3 = entradaUsuario;
-            break;
-        }
-        default:
-            alert("Ups! Algo salió mal...")
-    }
 
-    if(entradaUsuario < menorPrecio)
-        menorPrecio = entradaUsuario;
+    console.log("Los precios ingresados son:\n" + precio1 + " $\n" + precio2 + " $\n" + precio3 + " $");
+    console.log(" El menor de ellos es: " + menorPrecio + " $\n\t\tY el mayor es: " + obtenerElMayor(precio1,precio2,precio3) + " $\n");
+}
 
-    // if(entradaUsuario > mayorPrecio)
-    //     mayorPrecio = entradaUsuario;
-    }
-
-
-console.log("Los precios ingresados son:\n" + precio1 + " $\n" + precio2 + " $\n" + precio3 + " $");
-console.log(" El menor de ellos es: " + menorPrecio + " $\n\t\tY el mayor es: " + obtenerElMayor(precio1,precio2,precio3) + " $\n");
