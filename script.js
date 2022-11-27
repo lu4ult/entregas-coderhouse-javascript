114a125387c86d78d1907f61b1a6e919dcdb46e6
Lautaro <lu4ult@gmail.com>
2022-11-27 18:00:59 -0300

    add email.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 /**********************     VARIABLES GENERALES Y OBJETOS      ***********************************************************************************/
 let produccion = "https://friendly-bublanina-3c840e.netlify.app/" === window.location.href || "https://lu4ult.github.io/entregas-coderhouse-javascript/" === window.location.href;
+let emailDelUsuario = "lu4ult@gmail.com";
+
 
 let editSvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>'
 let deleteSvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M175 175C184.4 165.7 199.6 165.7 208.1 175L255.1 222.1L303 175C312.4 165.7 327.6 165.7 336.1 175C346.3 184.4 346.3 199.6 336.1 208.1L289.9 255.1L336.1 303C346.3 312.4 346.3 327.6 336.1 336.1C327.6 346.3 312.4 346.3 303 336.1L255.1 289.9L208.1 336.1C199.6 346.3 184.4 346.3 175 336.1C165.7 327.6 165.7 312.4 175 303L222.1 255.1L175 208.1C165.7 199.6 165.7 184.4 175 175V175zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>'
@@ -233,6 +235,16 @@ function llamarApiMeli(idMeli) {
             setTimeout(() => {
                 showToastiFy(_estado, str);
             },100);
+
+            //sendEmail("lautatourn@gmail.com",_titulo,_estado?"activo":"pausado");
+            
+            emailjs.send('service_k3tj0b9', 'template_gqsipms', {destinatario:emailDelUsuario,producto:_titulo,estado:_estado?"activo":"pausado"})
+            .then(function(response) {
+                console.log('Email enviado correctamente!', response.status, response.text);
+            }, function(error) {
+                console.log('Email fallo...', error);
+            });
+            
             //objetoActual.estado = _estado;
         }
         objetoActual.estado = _estado;
@@ -560,7 +572,6 @@ function simulacion() {
             reconstruirDom();
         }
       });
-
 }
 
 
