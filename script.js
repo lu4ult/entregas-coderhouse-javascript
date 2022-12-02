d19d901999ec706a1244a01fdf584ec71acf6a3d
Lautaro <lu4ult@gmail.com>
2022-12-02 01:08:47 -0300

    estilos en inputs de entrada, barra de búsqueda, textos descriptivos del objetivo

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let configuracionUsuario;
 let debugEnabled = false;
 let notificacionesPausadasMomentaneamente=false;
 let primeraCarga = true;
+let pausarLlamadaApi = false;
 
 let editSvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>'
 let deleteSvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M175 175C184.4 165.7 199.6 165.7 208.1 175L255.1 222.1L303 175C312.4 165.7 327.6 165.7 336.1 175C346.3 184.4 346.3 199.6 336.1 208.1L289.9 255.1L336.1 303C346.3 312.4 346.3 327.6 336.1 336.1C327.6 346.3 312.4 346.3 303 336.1L255.1 289.9L208.1 336.1C199.6 346.3 184.4 346.3 175 336.1C165.7 327.6 165.7 312.4 175 303L222.1 255.1L175 208.1C165.7 199.6 165.7 184.4 175 175V175zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>'
@@ -464,10 +465,12 @@ inputPresupuesto.onfocus = () => {
 
 botonPresupuesto.addEventListener("click",presupuesto);
 function presupuesto() {
+    pausarLlamadaApi = true;
+    setTimeout(() => {pausarLlamadaApi=false;},15*1000)
+
     let presupuestoUsuario = inputPresupuesto.value;
     if(isNaN(presupuestoUsuario)) {
         inputPresupuesto.value = "Ingrese sólo números";
-        console.log("mal")
         return;
     }
 
@@ -488,8 +491,7 @@ function presupuesto() {
         document.getElementById("productosContenedor").innerHTML = "";
         cargarProductoADom(productos[0]);
         inputPresupuesto.classList.add("input-error");
-        inputPresupuesto.value = "Necesita "+ (productos[0].precio - presupuestoUsuario) + "$ para comprar al menos " + productos[0].titulo;
-        console.log("Necesita "+ (productos[0].precio - presupuestoUsuario) + "$ para comprar al menos " + productos[0].titulo);
+        inputPresupuesto.value = "Falta al menos "+ (productos[0].precio - presupuestoUsuario) + "$";
     }
     else {
         //Escenario 2:
@@ -498,16 +500,13 @@ function presupuesto() {
             sumatoriaTotal += el.precio;
         }
         if(sumatoriaTotal <= presupuestoUsuario) {
-            //console.log("Felicidades! Le alcanza para comprar todo!");
             reconstruirDom();
-            inputPresupuesto.value = "Felicidades! Le alcanza para comprar todo!";
-            inputPresupuesto.classList.add("input-success")
-            //document.getElementById("productosContenedor").innerHTML = "Alcanza para todo";
+            inputPresupuesto.value = "Felicidades! Alcanza para todo!";
+            inputPresupuesto.classList.add("input-success");
         }
 
         //Escenario 3;
         else {
-            console.log("escenario 3");
             document.getElementById("productosContenedor").innerHTML = "";          //Eliminamos los productos para mostrar sólo los que alcanza el presupuesto.
             let indice = 0;
             let costoParcialCarrito = 0;
@@ -515,7 +514,7 @@ function presupuesto() {
             do {                                                                                    //Hacemos un do-while en lugar de while ya que alcana para almenos el primer producto (escenario 1).
                 costoParcialCarrito += productos[indice].precio;
                 //console.log("costo carrito: "+costoParcialCarrito);
-                mensajeParaUsuario += ", "+productos[indice].titulo;
+                //mensajeParaUsuario += ", "+productos[indice].titulo;
                 //productosSegunPresupuesto.push(productos[indice]);
                 cargarProductoADom(productos[indice]);
                 //console.log(productosSegunPresupuesto);
@@ -523,7 +522,8 @@ function presupuesto() {
             }while((costoParcialCarrito + productos[indice].precio) <= presupuestoUsuario);         //Mientras que el costo del carrito sumado al siguiente producto sea menor al presupuesto.
             inputPresupuesto.classList.add("input-success")
             //inputPresupuesto.value = "Su presupuesto alcanza para: " + mensajeParaUsuario + " Y sobran " + (presupuestoUsuario - costoParcialCarrito) + " $";
-            inputPresupuesto.value = "Su presupuesto alcanza sólo para los siguientes productos y sobran " + (presupuestoUsuario - costoParcialCarrito) + "$";
+            //inputPresupuesto.value = "Su presupuesto alcanza sólo para los siguientes productos y sobran " + (presupuestoUsuario - costoParcialCarrito) + "$";
+            inputPresupuesto.value = "Su presupuesto alcanza sólo para:";
         }
     }
 }
@@ -581,7 +581,8 @@ let botonDescargarCsv = document.getElementById("botonDescargarCsv");
 botonDescargarCsv.onclick = () => {
 
     //Para poder descargar un archivo como CSV básicamente creamos una matriz bidimensional y luego se codifica y descarga con un <a>
-    let textosDeNotas = [];
+     let textosDeNotas = [["id","titulo","precio","estado","fecha"]];
+    //let textosDeNotas = [];
     for(let el of productos) {
         let nuevoArray = [el.idMeli,el.titulo,el.precio,el.estado?"ACTIVO":"INACTIVO",obtenerFechaFormateada(el.fecha)];
         textosDeNotas.push(nuevoArray);
@@ -596,7 +597,7 @@ botonDescargarCsv.onclick = () => {
     let encodedUri = encodeURI(csvContent);
     let anchorDescarga = document.getElementById("anchorDescargar");
     anchorDescarga.setAttribute("href", encodedUri);                            //Para poder descargar el archivo creado hay que "adjuntarlo" a un anchor, el cuál no está visible en el DOM.
-    anchorDescarga.setAttribute("download", "productos.csv");
+    anchorDescarga.setAttribute("download", "tenes-stock_productos.csv");
     anchorDescarga.click(); // This will download the data file named "my_data.csv".
 
 };
@@ -668,6 +669,14 @@ document.getElementById('inputFile').addEventListener('change', function() {
 })
 
 
+let inputBusqueda = document.getElementById("inputBusqueda");
+inputBusqueda.onfocus = () => {
+    inputBusqueda.value = "";
+}
+inputBusqueda.onchange = () => {
+    console.log(inputBusqueda.value);
+}
+
 /**********************     MENÚ CONFIGURACIÓN DEL USUARIO ********************************************************************/
 
 let inputNotificPopUp = document.getElementById("notificPopUp");
@@ -715,7 +724,26 @@ window.addEventListener('keydown', function (e) {
     if(e.key === "Escape") {
         document.getElementById("configuracionUsuarioContenedor").classList.add("oculto");
         document.getElementById("notificacionesContenedor").classList.add("oculto");
+        document.getElementById("inputBusqueda").blur();                                                //Soltar focus del input sino se vuelve a filtrar
+        document.getElementById("inputBusqueda").value = "Buscar por título en las publicaciones";
+        reconstruirDom();
     }
+});
+
+//Utilizamos keyUp y no keyDown para el input de busqueda porque sino "me como" el último caracter
+window.addEventListener('keyup', function (e) {
+    if(document.getElementById("inputBusqueda") === document.activeElement) {
+        let arrayProductosEncontrados = [];
+        let textoActual = document.getElementById("inputBusqueda").value.toLowerCase();
+        productos.forEach(e => {
+            if(e.titulo.toLowerCase().indexOf(textoActual) >=0)
+                arrayProductosEncontrados.push(e);
+        });
+        document.getElementById("productosContenedor").innerHTML = "";
+        arrayProductosEncontrados.forEach(e => cargarProductoADom(e));
+
+    }
+
 });
 
 /***  MODAL  ***/
@@ -760,12 +788,6 @@ function simulacion() {
       });
 }
 
-
-// let inputBusqueda = document.getElementById("inputBusqueda");
-
-// inputBusqueda.addEventListener("onchange", () => {
-//     console.log(inputBusqueda.value)
-// })
 /******************************************************************************************************************************/
 console.clear();
 console.log("%cHola!","color:blue;font-size:2.5rem;border-bottom:1px solid blue;")
@@ -788,6 +810,9 @@ let intervaloLlamadaAPi = setInterval(() => {
     if(contadorLlamadasApi >= 36) {
         clearInterval(intervaloLlamadaAPi);
     }
+    if(pausarLlamadaApi === true)
+        return;
+
     productos.forEach(e => {
         setTimeout(() => {llamarApiMeli(e.idMeli);},Math.floor(Math.random()*300))
     });
